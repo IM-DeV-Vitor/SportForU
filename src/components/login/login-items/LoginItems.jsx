ea99e7e152aeb7c6effbee060b09f01b1b9efb5c
DeV-Vitor <vitorhugo1235lll@gmail.com>
2025-05-05 07:13:42 -0400

    Fim

--- src/components/login/login-items/LoginItems.jsx
+++ src/components/login/login-items/LoginItems.jsx
@@ -5,8 +5,8 @@ export default function LoginItems() {
     console.log("Login Items loaded")
     return ( 
             <div className={styles.container}>
-                <img src={imageLogo} alt="Logo SportForU" id={styles.logo}/>
                 <h1>Login</h1>
+                <img src={imageLogo} alt="Logo SportForU" id={styles.logo}/>
                 <div className={styles.inputsContainer}>
                     <input type="email" name="email" id={styles.email} required placeholder="Email: "/>
                     <input type="password" name="password" id={styles.password} required placeholder="Password: "/>
