d43aca4198843e29f1450bbcbe11bb6c30e37860
DeV-Vitor <vitorhugo1235lll@gmail.com>
2025-05-05 14:59:16 -0400

    manuntenção

--- src/components/login/login-items/LoginItems.jsx
+++ src/components/login/login-items/LoginItems.jsx
@@ -16,7 +16,7 @@ export default function LoginItems() {
     const handleLogin = async (e) => {
         e.preventDefault()
         try {
-            const response = await fetch("http://localhost:8080/auth/login", {
+            const response = await fetch("https://sportforu-backend.onrender.com/auth/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
